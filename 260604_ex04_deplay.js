d2b88554aa3b339ebf13e16221e1d5fa0a8a56a2
goarns77 <goarns771@gmail.com>
2026-06-04 14:37:08 +0900

    feat: implement chatbot interface with multi-provider support for Google and Groq

---
+++ 260604_ex04_deplay.js
@@ -0,0 +1,94 @@
+//디펜던시
+require ("dotenv").config();
+const express = require("express");
+const {GoogleGenAI} = require("@google/genai");
+const GroqAI = require("groq-sdk");
+const path = require("path");
+
+//전역 변수
+const app = express();
+const google = new GoogleGenAI({apiKey: process.env.GOOGLE_API_KEY});
+const groq = new GroqAI({apiKey: process.env.GROQ_API_KEY});
+const PORT = 3007;
+
+app.use(express.json());
+
+app.use(
+    express.static(path.join(__dirname,"public"))
+);
+
+app.post("/chat", async (req,res)=>{
+    //입력
+    const {
+        provider,
+        model,
+        ask,
+    } = req.body;
+    //로직
+    let result;
+    switch(true){
+        case provider === "Google":
+            console.log("Google Provider Requested!");
+            result = useGoogle(model, ask);
+            break;
+        case provider === "Groq":
+            console.log("Groq Provider Requested!");
+            result = useGroq(model, ask);
+            break;
+        default:
+            console.log("Unknown Provider (Wrong Request)")
+            res.status(404).json({msg : "Provider not exist"})
+            break;
+    }
+    //출력(JSON)
+    res.json({
+        provider,
+        model,
+        ask,
+    })
+})
+
+app.post("/chat", async (req, res) => {
+  // 입력 (JSON)
+  const { provider, model, ask } = req.body;
+  // 로직 (AI Provider)
+  let result;
+  switch (true) {
+    case provider === "google":
+      console.log("google 제공자 요청");
+      result = await useGoogle(model, ask);
+      break;
+    case provider === "groq":
+      console.log("groq 제공자 요청");
+      result = await useGroq(model, ask);
+      break;
+    default:
+      console.log("잘못된 Provider");
+      res.status(404).json({ msg: "존재하지 않는 Provider" });
+      return;
+  }
+  // 출력 (JSON)
+  res.json({
+    result,
+  });
+});
+
+async function useGoogle(model, ask) {
+  const response = await google.models.generateContent({
+    model, // 못 쓰는 모델은 예외처리될 예정
+    contents: ask,
+  });
+  return response.text;
+}
+
+async function useGroq(model, ask) {
+    const response = await groq.chat.completions.create({
+        messages: [{ role: "user", content: ask }],
+        model,
+    });
+    return response.choices[0].message.content;
+}
+
+app.listen(PORT, ()=>{
+    console.log(`${PORT} has been connected.`);
+})
